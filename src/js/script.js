54e41abc2b20d639cb377c9ede681354615a1819
Sasha Volkov <sashavolkov_03@mail.ru>
2023-02-18 15:08:32 +0400

    slider end

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//       speed: 900,
+//       adaptiveHeight: true,
+//       prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron left solid.png"></button>',
+//       nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron right solid.png"></button>',
+//       responsive: [
+//         {
+//           breakpoint: 992,
+//           settings: {
+//             dots: true, 
+//             arrows: false
+//           }
+//         }
+//       ]
+//       /*autoplay: true,
+//       autoplaySpeed: 1900 */
+//     });
+//   });
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  // autoplay: true, 
+  // autoplaySpeed: 100,
+  controls: false,
+  nav: false,
+});
+
+document.querySelector('.prev').addEventListener('click' , function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click' , function () {
+  slider.goTo('next');
+});
+
