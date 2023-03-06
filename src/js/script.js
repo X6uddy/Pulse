ca697254afb0e661c44206e9462841582c95472b
Sasha Volkov <sashavolkov_03@mail.ru>
2023-03-06 05:42:00 +0400

    mb start working harder. my laziness is killing me

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,39 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//       speed: 900,
-//       adaptiveHeight: true,
-//       prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron left solid.png"></button>',
-//       nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron right solid.png"></button>',
-//       responsive: [
-//         {
-//           breakpoint: 992,
-//           settings: {
-//             dots: true, 
-//             arrows: false
-//           }
-//         }
-//       ]
-//       /*autoplay: true,
-//       autoplaySpeed: 1900 */
-//     });
-//   });
+$(document).ready(function(){
+  // $('.carousel__inner').slick({
+  //     speed: 1200,
+  //     adaptiveHeight: true,
+  //     prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+  //     nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+  //     responsive: [
+  //         {
+  //             breakpoint: 992,
+  //             settings: {
+  //                 dots: true,
+  //                 arrows: false
+  //             }
+  //         }
+  //     ]
+  // });
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })    
+    })
+  }
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+});
+
+
 
 const slider = tns({
   container: '.carousel__inner',
@@ -34,4 +51,3 @@ document.querySelector('.prev').addEventListener('click' , function () {
 document.querySelector('.next').addEventListener('click' , function () {
   slider.goTo('next');
 });
-
